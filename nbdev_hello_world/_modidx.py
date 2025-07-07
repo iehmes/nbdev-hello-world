994f159fa9f6f7fdd897252e0a50269e5d817bd5
Martin Schönecker <m-schoenecker@web.de>
2025-07-07 20:21:41 +0200

    say_hello

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://iehmes.github.io',
                 'git_url': 'https://github.com/iehmes/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': { 'nbdev_hello_world.core.foo': ('meins.html#foo', 'nbdev_hello_world/core.py'),
+                                        'nbdev_hello_world.core.say_hello': ('meins.html#say_hello', 'nbdev_hello_world/core.py')}}}
